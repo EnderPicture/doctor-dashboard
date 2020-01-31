1b1eed1964fb556c83b2c57bfcbff48b55a58c00
ridge zhang <ridgez@sfu.ca>
2020-01-31 15:11:13 -0800

    added more data for abdominal pain

---
+++ AbdominalPainData/MediTech_EDM.js
@@ -0,0 +1,10 @@
+[
+{CTAS: "3",},
+{Chief_Complaint: "Abdo Pain,Mod,+/- Episod N&V",},
+{Depart_Disposition: "Admitted",},
+{Arrival_Time: "02-Dec-2030 15:23",},
+{Triage_Time: "02-Dec-2030 15:28",},
+{Physician_Initial_Assessment_Time: "02-Dec-2030 15:43",},
+{Admitted_Time: "02-Dec-2030 16:42",},
+
+]
