ec99af5fefab372344fe3ce8d20687414ee35998
ridge zhang <ridgez@sfu.ca>
2020-02-26 11:07:06 -0800

    sample units for sample tests

---
+++ sample_tests.js
@@ -0,0 +1,108 @@
+let sample_tests = [
+    {
+        CBC: [
+            //https://www.mayoclinic.org/tests-procedures/complete-blood-count/about/pac-20384919
+            {
+                type: "Red Blood Cell Count",
+                range: "4.35-5.65",
+                units: "cells/L",
+
+            },
+            {
+                type: "Hemoglobin",
+                range: "13.2-16.6",
+                units: "grams/dL",
+
+            },
+            {
+                type: "Hematocrit",
+                range: "38.3-48.6",
+                units: "percent",
+
+            },
+            {
+                type: "White blood cell count",
+                range: "3.4-9.6",
+                units: "billion cells/L",
+
+            },
+            {
+                type: "Platelet count",
+                range: "135-317",
+                units: "billion/L",
+
+            },
+        ],
+        Chem7: [
+            //https://www.ucsfbenioffchildrens.org/tests/003462.html
+            {
+                type: "BUN",
+                value: "7-20",
+                range: "mg/dL",
+            },
+            {
+                type: "CO2",
+                value: "20 to 29",
+                range: "mmol/L",
+            },
+            {
+                type: "Creatinine",
+                value: "0.8 to 1.4",
+                range: "mg/dL",
+            },
+            {
+                type: "Glucose",
+                value: "64 to 128",
+                range: "mg/dL",
+            },
+            {
+                type: "Serum chloride",
+                value: "101 to 111",
+                range: "mmol/L",
+            },
+            {
+                type: "Serum potassium",
+                value: "3.7 to 5.2",
+                range: "mEq/L",
+            },
+            {
+                type: "Serum sodium",
+                value: "136 to 144",
+                range: "mEq/L",
+            },
+        ],
+        Magnesium: [
+            //https://medlineplus.gov/ency/article/003487.htm
+            {
+            value: "1.7 to 2.2",
+            units: "mg/dL",
+            }
+
+        ],
+        Phosphorus: [
+            //https://medlineplus.gov/ency/article/003487.htm
+            {
+            value: "3.4 to 4.5",
+            units: "mg/dl",
+            }
+
+        ],
+        Phosphorus: [
+            //https://www.ncbi.nlm.nih.gov/books/NBK310/
+            {
+            value: "3.4 to 4.5",
+            units: "mg/dl",
+            }
+
+        ],
+        PartialThromboplastinTime: [
+            //https://emedicine.medscape.com/article/2085837-overview
+            {
+            value: "30-40",
+            units: "mg/seconds",
+            }
+
+        ],
+
+    }
+]
